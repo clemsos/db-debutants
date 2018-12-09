7204e2d1a6706590143f8bac63325cd9561fb596
Clement Renaud <clement.renaud@gmail.com>
2018-12-09 14:53:14 +0100

    script to import artists in db

---
+++ music/4-music_import_artists.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import csv
+import sqlite3
+from json import dumps
+
+db = sqlite3.connect('../chinook/chinook.db')
+print 'Database connectée'
+
+with open('music.csv') as csv_file:
+
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    for i, row in enumerate(csv_reader):
+        if i != 0:
+            artist_name = row[2]
+
+            encoded = dumps(artist_name)
+
+            # see https://stackoverflow.com/questions/36765835/inserting-text-having-single-quote-in-sqlite-database
+            requete = ''' INSERT INTO artists (name) VALUES (?); '''
+
+            # insert artist name into artists tables
+            db.execute(requete, (artist_name,))
+
+    db.commit()
+
+db.close()
+print 'Database fermée'
